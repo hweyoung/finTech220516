5cd5998a52d57ef1e2d78f262e7349905212c379
hweyoung <gnldud0516@gmail.com>
2022-05-18 14:15:51 +0900

    new title, content, img

--- fintech/src/news/SearchResult.js
+++ fintech/src/news/SearchResult.js
@@ -1,12 +1,19 @@
 // APIkeys : e923ac1e2bd04192bfc06cba467a5531
 import React from 'react'
+import styled from 'styled-components'
+import SearchResultCard from './SearchResultCard'
+
+
+const SearchResultBlock = styled.div`
+    margin : 10px
+`
 
 const SearchResult = ({searchList}) => { 
     return (
         <>
             {
-            searchList.map(({title})=>{
-                return (<p>{title}</p>)
+            searchList.map(({title,urlToImage, url})=>{
+                return (<SearchResultCard title={title} imageUrl = {urlToImage} link={url}></SearchResultCard>)
             })
         }
         </>
