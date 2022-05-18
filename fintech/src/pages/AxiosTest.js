b24cad146cb8c71b896ed10982e9fd60afedf176
hweyoung <gnldud0516@gmail.com>
2022-05-18 11:17:21 +0900

    api Test 배열에서 data 가져오기

--- fintech/src/pages/AxiosTest.js
+++ fintech/src/pages/AxiosTest.js
@@ -1,22 +1,28 @@
 import React, { useState } from 'react'
 import AppBar from '../components/common/AppBar';
-import axios from 'axios'
+import axios from 'axios';
 
 const AxiosTest = () => {
-  const [data, setData] = useState();
-  const handleClick = () => {
-    console.log('hello');
-    axios.get("https://naver.com").then((data)=>{
-        console.log(data)
-    })
-  }  
-
-  return (
-    <>
-      <AppBar title={"통신테스트"}/>
-      <button onClick={handleClick}>데이터 Request(요청)</button>
-    </>
-  )
-}
-
-export default AxiosTest
+    const [data, setData] = useState([]);
+    const handleClick = () => {
+      axios.get('https://api.sampleapis.com/coffee/hot').then((response) => {
+          console.log(response.data);
+          setData(response.data)
+      });
+      console.log("hello");
+    }  
+  
+    return (
+      <>
+        <AppBar title={"통신테스트"}/>
+        <button onClick={handleClick}>데이터 Request(요청)</button>
+        {
+            data.map((coffee) => {
+                return <p>{coffee.title}</p>
+            })
+        }
+      </>
+    )
+  }
+  
+  export default AxiosTest
