a46eadee3f7dbd4d2ea49d085f9699dc334ea491
hweyoung <gnldud0516@gmail.com>
2022-05-19 11:49:43 +0900

    사용자 인증 후 /authResult 에서  accessToken 발급받고 localstorage에 저장

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -3,16 +3,18 @@ import Welcome from "./components/Welcome";
 import ArrayMap from "./components/ArrayMap";
 import AxiosTest from "./pages/AxiosTest";
 import NewsApiPage from "./pages/NewsApiPage";
+import AuthPage from "./pages/AuthPage";
+import AuthResultPage from "./pages/AuthResultPage"
 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/a" element={<Welcome/>}></Route>
-        <Route path="/b" element={<ArrayMap/>}></Route>
         <Route path="/axiosTest" element={<AxiosTest/>}></Route>
         <Route path="/newsApi" element={<NewsApiPage/>}></Route>
+        <Route path="/" element={<AuthPage/>}></Route>
+        <Route path="authResult" element={<AuthResultPage/>}></Route>
       </Routes>
     </BrowserRouter>
   );
